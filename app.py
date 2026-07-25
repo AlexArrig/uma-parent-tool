87ec295d63ccd89afe6eda0fb260292f75151674
AlexArrig <alex.arrig@gmail.com>
2026-07-25 14:54:01 +0200

    More brackets part 2

--- app.py
+++ app.py
@@ -231,7 +231,7 @@ def index():
                         combined_white[name] = f
             for p in v1.get("localized_parents", []) + v2.get("localized_parents", []):
                 for pf in p.get("factors", []):
-                    if pf.get("category"] == "white":
+                    if pf.get("category") == "white":
                         name = pf["name"]
                         if name not in combined_white or pf["star"] > combined_white[name]["star"]:
                             combined_white[name] = pf
